9e837dc6be34558fb0ffaf3e317b27ae243e57ab
Jordan Chapman <jchapman63@capital.edu>
2023-03-13 16:49:30 -0400

    project creation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* this here header thing is making this nice background color happen */}
+      <header className="App-header">
+        <h1>College University</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

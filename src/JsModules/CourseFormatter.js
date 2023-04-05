289db40478c859e57bec54c627a0c2405c9f9613
Jordan Chapman <jchapman63@capital.edu>
2023-04-05 18:58:05 -0400

    implemented CourseFormatter.js so that Courses take up a proportion of cells relative to their time length

---
+++ src/JsModules/CourseFormatter.js
@@ -0,0 +1,143 @@
+// this module formats the data of course so that
+// it can be displayed in the UI with proper proportioning
+// it will take a course as an argument and create an
+// html element for it
+
+// using this module I can:
+// format the course time to a corresponding cell ID: this.standardFormat
+// get a list of which child elements of the row get the course: this.courseDays
+// create a properly sized html element for the course: this.createCourseElement
+
+class CourseFormatter {
+  constructor(course) {
+    this.course = course;
+    // used for mapping meetFrequencies to a cell
+    this.days = {
+      M: 1,
+      T: 2,
+      W: 3,
+      Th: 4,
+      F: 5,
+    };
+  }
+
+  // format the string to match table IDs
+  standardFormat = () => {
+    var meetTime = this.course.meetTime;
+    // console.log("meet time of course", meetTime);
+
+    var startNumber = meetTime.slice(0, 2);
+    // console.log("start number grabbed", startNumber);
+
+    var startLetter = meetTime.slice(5, 6);
+    // console.log("start letter grabbed", startLetter);
+
+    return startNumber + startLetter;
+  };
+
+  courseDays = () => {
+    var courseDays = this.course.meetFrequencies.split(" ");
+    // console.log("days as a list:", courseDays);
+
+    for (let i = 0; i < courseDays.length; i++) {
+      const key = courseDays[i];
+      if (this.days.hasOwnProperty(key)) {
+        courseDays[i] = this.days[key];
+      }
+    }
+
+    return courseDays;
+  };
+
+  // determin the height of course in schedule table cell
+  _courseHeight = () => {
+    const startTime = this.course.meetTime.slice(0, 5);
+    const endTime = this.course.meetTime.slice(8, 13);
+    // console.log("start time", startTime);
+    // console.log("end time", endTime);
+
+    // make ints for startHour
+    if (startTime[0] === "0") {
+      var startHour = parseInt(startTime[1]);
+      // console.log("start hour", startHour);
+    } else {
+      startHour = parseInt(startTime.slice(0, 2));
+      // console.log("start hour", startHour);
+    }
+
+    // make int for startMinutes
+    if (startTime[3] === "0") {
+      var startMinutes = parseInt(startTime[4]);
+      // console.log("start minutes", startMinutes);
+    } else {
+      startMinutes = parseInt(startTime.slice(3, 5));
+      // console.log("start minutes", startMinutes);
+    }
+
+    // make int for endHour
+    if (startTime[0] === "0") {
+      var endHour = parseInt(endTime[1]);
+      // console.log("end hour", endHour);
+    } else {
+      endHour = parseInt(endTime.slice(0, 2));
+      // console.log("end hour", endHour);
+    }
+
+    // make int for endMinutes
+    if (endTime[3] === "0") {
+      var endMinutes = parseInt(endTime[4]);
+      // console.log("start minutes", endMinutes);
+    } else {
+      endMinutes = parseInt(endTime.slice(3, 5));
+      // console.log("start minutes", endMinutes);
+    }
+
+    // calculate height %
+
+    // convert to 24 hour time
+    if (this.course.meetTime.slice(5, 6) === "P") {
+      startHour += 12;
+    }
+    // console.log(course.meetTime.slice(13, 14));
+    if (this.course.meetTime.slice(13, 14) === "P") {
+      endHour += 12;
+    }
+
+    // calculate differences
+    var hourDifference = (endHour - startHour) * 100;
+    // console.log("endHour", endHour);
+    // console.log("startHour", startHour);
+    // console.log("hour difference", hourDifference);
+    var minuteDifference = endMinutes - startMinutes;
+    if (minuteDifference < 0) {
+      minuteDifference += 60;
+    }
+    minuteDifference /= 60;
+
+    const sizePercentage = (hourDifference + minuteDifference) * 100;
+
+    // console.log("new size", sizePercentage);
+
+    return sizePercentage;
+  };
+
+  // create the actual course element
+  createCourseElement = () => {
+    // create the element
+    var newElement = document.createElement("div");
+
+    // add its css class
+    newElement.classList.add("course-box");
+
+    // add the course information
+    newElement.innerHTML = this.course.courseName;
+
+    const size = this._courseHeight(this.course);
+
+    newElement.style.setProperty("--height", size);
+
+    return newElement;
+  };
+}
+
+export default CourseFormatter;
--- src/homeComponents/Calendar.js
+++ src/homeComponents/Calendar.js
@@ -1,131 +1,41 @@
 import "./componentStyles/calendar.css";
+import CourseFormatter from "../JsModules/CourseFormatter";
 
 function Calendar(props) {
-  // console.log("courses available", props.currentSchedule);
-
-  const days = {
-    M: 1,
-    T: 2,
-    W: 3,
-    Th: 4,
-    F: 5,
-  };
-
-  const standardFormat = (course) => {
-    var meetTime = course.meetTime;
-    // console.log("meet time of course", meetTime);
-
-    var startNumber = meetTime.slice(0, 2);
-    // console.log("start number grabbed", startNumber);
-
-    var startLetter = meetTime.slice(5, 6);
-    // console.log("start letter grabbed", startLetter);
-
-    return startNumber + startLetter;
-  };
-
-  const courseDays = (course) => {
-    var courseDays = course.meetFrequencies.split(" ");
-    // console.log("days as a list:", courseDays);
-
-    for (let i = 0; i < courseDays.length; i++) {
-      const key = courseDays[i];
-      if (days.hasOwnProperty(key)) {
-        courseDays[i] = days[key];
-      }
-    }
-
-    return courseDays;
-  };
-
-  const courseHeight = (course) => {
-    const startTime = course.meetTime.slice(0, 5);
-    const endTime = course.meetTime.slice(8, 13);
-    console.log("start time", startTime);
-    console.log("end time", endTime);
-
-    // make ints for startHour
-    if (startTime[0] === "0") {
-      var startHour = parseInt(startTime[1]);
-      console.log("start hour", startHour);
-    } else {
-      startHour = parseInt(startTime.slice(0, 2));
-      console.log("start hour", startHour);
-    }
-
-    // make int for startMinutes
-    if (startTime[3] === "0") {
-      var startMinutes = parseInt(startTime[4]);
-      console.log("start minutes", startMinutes);
-    } else {
-      startMinutes = parseInt(startTime.slice(3, 5));
-      console.log("start minutes", startMinutes);
-    }
-
-    // make int for endHour
-    if (startTime[0] === "0") {
-      var endHour = parseInt(endTime[1]);
-      console.log("end hour", endHour);
-    } else {
-      endHour = parseInt(endTime.slice(0, 2));
-      console.log("end hour", endHour);
-    }
-
-    // make int for endMinutes
-    if (endTime[3] === "0") {
-      var endMinutes = parseInt(endTime[4]);
-      console.log("start minutes", endMinutes);
-    } else {
-      endMinutes = parseInt(endTime.slice(3, 5));
-      console.log("start minutes", endMinutes);
-    }
-  };
-  const createCourseElement = (course) => {
-    // create the element
-    var newElement = document.createElement("div");
-
-    // add its css class
-    newElement.classList.add("course-box");
-
-    // add the course information
-    newElement.innerHTML = course.courseName;
-
-    courseHeight(course);
-
-    return newElement;
-  };
-
+  // make sure a schedule is passed
   if (props.currentSchedule) {
     if (props.currentSchedule.courses) {
       // eslint-disable-next-line array-callback-return
       props.currentSchedule.courses.map((course) => {
+        // initialize formatter object
+        const courseFormatter = new CourseFormatter(course);
+
         // format the start time of the course
-        var formattedStartTime = standardFormat(course);
-        console.log("formatted start time:", formattedStartTime);
+        var courseStartFormatted = courseFormatter.standardFormat();
 
         // format the days
-        var meetDays = courseDays(course);
-        console.log("formatted days array", meetDays);
+        var formattedMeetDays = courseFormatter.courseDays();
+        // console.log("formatted days array", meetDays);
 
         // grab the parent element, equivalent ID to formattedStartTime
-        var parentElem = document.getElementById(formattedStartTime);
+        var parentElem = document.getElementById(courseStartFormatted);
 
         // grab the children
         var children = parentElem.children;
 
         // create an element for the course
-        var courseElem = createCourseElement(course);
+        var formattedCourseElement = courseFormatter.createCourseElement();
 
         // for each day
-        for (let i = 0; i < meetDays.length; i++) {
+        for (let i = 0; i < formattedMeetDays.length; i++) {
           // find which day needs to be created
-          var day = meetDays[i];
+          var day = formattedMeetDays[i];
 
           // find the corresponding element
           var elem = children[day];
 
           // add the course elment to the table element
-          elem.innerHTML = courseElem.outerHTML;
+          elem.innerHTML = formattedCourseElement.outerHTML;
         }
       });
     }
